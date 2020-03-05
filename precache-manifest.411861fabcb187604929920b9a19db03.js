387bbf938c200dbc883e273820176d4f27d60f39
Gauthamjm007 <j.mgautham15@gmail.com>
2020-03-05 11:38:19 +0530

    Updates

---
+++ precache-manifest.411861fabcb187604929920b9a19db03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b7732febec473fbf7594df7700b5b6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "45cd5c3d6d60cd125eb7",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "aa714904883e6c6e39b6",
+    "url": "/portfolio/static/css/main.b111f6ad.chunk.css"
+  },
+  {
+    "revision": "45cd5c3d6d60cd125eb7",
+    "url": "/portfolio/static/js/2.734ad3c6.chunk.js"
+  },
+  {
+    "revision": "4482739f3a8ff521b2955664b1fec844",
+    "url": "/portfolio/static/js/2.734ad3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa714904883e6c6e39b6",
+    "url": "/portfolio/static/js/main.cd3ea6f5.chunk.js"
+  },
+  {
+    "revision": "c4322a7adae57c8cb5cd",
+    "url": "/portfolio/static/js/runtime-main.dbf595e6.js"
+  }
+]);
